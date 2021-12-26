3adbfe8f27d6c993833f3a5881076cd36128bc64
Youcef <youcef@el-kamel.fr>
2021-12-26 14:45:09 +0100

    initial commit remove react-native code and add flutter init

--- index.android.js
+++
@@ -1,9 +0,0 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
- import { AppRegistry } from 'react-native'
- import App from './App/Containers/App'
-
- AppRegistry.registerComponent('SwiPolitique', () => App)
